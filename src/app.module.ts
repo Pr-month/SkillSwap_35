bb68c33a36f84abb5c53e850d48144161189c482
Duman Sadu <duman.saduakasov@yandex.ru>
2026-01-15 23:21:33 +0500

    Подключение jwt модуля глобально

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    JwtModule.register({
+      global: true, // JWT подключён глобально
+      secret: process.env.JWT_SECRET || 'jwt_secret',
+      signOptions: { expiresIn: '1h' },
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
