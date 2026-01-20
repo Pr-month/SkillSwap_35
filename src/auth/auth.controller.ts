8795bf51caa069a50b80e4144c216239869e174c
unknown <boneislandman00@gmail.com>
2026-01-20 16:46:59 +0400

    feat(auth): add login endpoint via auth/login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,10 +2,16 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { AuthService } from './auth.service';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { LoginAuthDto } from './dto/login-auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  login(@Body() loginAuthDto: LoginAuthDto) {
+    return this.authService.login(loginAuthDto);
+  }
 
   @Post()
   create(@Body() createAuthDto: CreateAuthDto) {
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { CreateAuthDto } from './dto/create-auth.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
+import { LoginAuthDto } from './dto/login-auth.dto';
 
 @Injectable()
 export class AuthService {
+  login({ email, password }: LoginAuthDto) {
+    // временная логика, без БД
+    if (email === 'test@test.com' && password === '123456') {
+      return {
+        message: 'Login successful',
+        user: { email },
+      };
+    }
+
+    throw new UnauthorizedException('Invalid credentials');
+  }
+  
   create(createAuthDto: CreateAuthDto) {
     return 'This action adds a new auth';
   }
