9be53797a625c2d43aaf65f7f05bfa3ed534846f
Duman Sadu <duman.saduakasov@yandex.ru>
2026-01-23 16:42:49 +0500

    Исправлен app.config с использованием registerAs и типизации

--- src/app.config.ts
+++
@@ -1,4 +0,0 @@
-export const appConfig = () => ({
-  port: Number(process.env.PORT) || 3000,
-  hashSalt: Number(process.env.HASH_SALT) || 10,
-});
--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,8 @@ import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 <<<<<<< Updated upstream
 =======
-import { appConfig } from './app.config';
+import { ConfigModule } from '@nestjs/config';
+import { appConfig } from './config/app.config';
 >>>>>>> Stashed changes
 
 @Module({
