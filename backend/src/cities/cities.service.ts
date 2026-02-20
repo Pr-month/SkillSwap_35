ea125db718a5df46ddbeb784c6aae21786623276
Lem0n4eLL0 <vladislavche@bk.ru>
2026-02-20 22:34:48 +0400

    Добавлен метод GET для получения всех городов

--- backend/src/cities/cities.service.ts
+++ backend/src/cities/cities.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { City } from './entities/city.entity';
+import { GetCitiesDto } from './dto/cities.dto';
 
 @Injectable()
 export class CitiesService {
@@ -10,4 +11,16 @@ export class CitiesService {
     private cityRepository: Repository<City>,
   ) {}
 
+  async findAll(query: GetCitiesDto) {
+    const { search, limit = 10 } = query;
+    const qb = this.cityRepository.createQueryBuilder('city');
+
+    qb.select(['city.id', 'city.name']);
+    if (search && search.trim()) {
+      qb.where('city.name ILIKE :search', { search: `%${search.trim()}%` });
+    }
+    qb.take(limit);
+
+    return qb.getMany();
+  }
 }
